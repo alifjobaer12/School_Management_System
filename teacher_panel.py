ff369faacebe7ef348ffd20401e28e1665a4e02e
alifjobaer12 <alifjobaer12@gmail.com>
2025-04-27 19:02:36 +0600

    add section in teacher routine

--- teacher_panel.py
+++ teacher_panel.py
@@ -20,17 +20,19 @@ db = mysql.connector.connect(
 cursor = db.cursor()
 
 
-
+# for log_out btn hover
 def hover_on(event, color, btn_name):
     btn_name.configure(text_color=color)
 
 def hover_off(event, color, btn_name):
     btn_name.configure(text_color=color)
 
+# main window
 login_window = ctk.CTk()
 login_window.geometry("700x400+1200+100")
 login_window.title("Login - School Management System")
 
+# main tec frame
 tec_panal_frame = CTkFrame(login_window, fg_color="sky blue", width=700, height=400)
 tec_panal_frame.place(x=350, y=200, anchor="center")
 
@@ -43,7 +45,7 @@ tec_logout.place(x=640, y=50, anchor="center")
 tec_logout.bind("<Enter>", lambda event: hover_on(event, "red", tec_logout))
 tec_logout.bind("<Leave>", lambda event: hover_off(event, "black", tec_logout))
 
-# student Info Frame
+# teacher Info Frame
 
 tec_info_view_frame = CTkFrame(tec_panal_frame, width=700, height=300, fg_color="transparent",)
 tec_info_view_frame.place(x=350, y=230, anchor="center")
@@ -64,22 +66,23 @@ label_colon.place(x=320, y=10)
 label_colon = ctk.CTkLabel(tec_info_view_frame, text=f":     {total_sub[0]}", text_color="black", font=("Helvetica",14, "bold"), anchor="w")
 label_colon.place(x=320, y=40)
 
-cursor.execute("SELECT sub_name, class, class_start_time, class_end_time  FROM subjects WHERE username = 'ashikuzzaman01';")
+# teacher class routine
+cursor.execute("SELECT sub_name, class, section, class_start_time, class_end_time  FROM subjects WHERE username = 'ashikuzzaman01';")
 results = cursor.fetchall()
 
 ruttin_lable = CTkLabel(tec_info_view_frame, text="Class Routine", text_color="black", width=1, height=1, fg_color="transparent", font=("Helvetica",13, "bold"))
 ruttin_lable.place(x=350, y=100, anchor="center")
 
 subject_box = CTkTextbox(tec_info_view_frame, wrap="none", activate_scrollbars=True, width=500, height=160, fg_color="transparent", scrollbar_button_color="sky blue", font=("Helvetica",13, "bold"),)
-subject_box.place(x=400, y=200, anchor="center")
+subject_box.place(x=390, y=200, anchor="center")
 
 subject_box.delete('0.0', 'end')
-header = "Subject\t\t\tClass\tStart Time\t\tEnd Time\n"
+header = "Subject\t\t\tClass\tSection\t   Start Time\t\tEnd Time\n"
 subject_box.insert('end', header)
-subject_box.insert('end', "-"*100 + '\n')
+subject_box.insert('end', "-"*115 + '\n')
 
 for info in results:
-    line = f"{info[0]}\t\t\t  {info[1]}\t {info[2]}\t\t {info[3]}\n"
+    line = f"{info[0]}\t\t\t  {info[1]}\t  {info[2]}\t     {info[3]}\t\t {info[4]}\n"
     subject_box.insert('end', line)
 subject_box.configure(state="disabled")
 
