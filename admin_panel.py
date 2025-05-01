f122f2320bd02540ee57e77431072d10b1326ee6
alifjobaer12 <alifjobaer12@gmail.com>
2025-05-01 14:40:22 +0600

    add backend sql in admin_panel

--- admin_panel.py
+++ admin_panel.py
@@ -65,9 +65,8 @@ class admin_panel:
         all_std_textbox = CTkTextbox(all_std_info, font=("Helvetica", 12))
         all_std_textbox.pack(fill="both", expand=True, side="top")
 
-        all_std_sql = "SELECT * FROM students;"
-        self.cursor.execute(all_std_sql)
-        see_info = self.cursor.fetchall()
+        # sql backend
+        see_info = self.sql.add_student()
 
         all_std_textbox.delete('0.0', 'end')
         header = "ID\tUsername\t\t\tName\t\t\tClass\tRoll\tSection\tGrade\tPhone\t\t\tAddress\t\t\tTuition Fee\t\tPaid Fee\n"
@@ -79,6 +78,28 @@ class admin_panel:
             all_std_textbox.insert('end', line)
 
         all_std_info.mainloop()
+
+    def see_all_tec_info(self):
+        all_tec_info = CTk()
+        all_tec_info.geometry("720x900")
+        all_tec_info.title("All Teacher Info")
+
+        all_std_textbox = CTkTextbox(all_tec_info, font=("Helvetica", 12))
+        all_std_textbox.pack(fill="both", expand=True, side="top")
+
+        # sql backend
+        see_info = self.sql.all_teachers()
+
+        all_std_textbox.delete('0.0', 'end')
+        header = "ID\tUsername\t\t\tName\t\t\tPhone\t\t\tAddress\n"
+        all_std_textbox.insert('end', header)
+        all_std_textbox.insert('end', "-"*175 + '\n')
+
+        for info in see_info:
+            line = f"{info[0]}\t{info[1]}\t\t\t{info[2]}\t\t\t{info[3]}\t\t\t{info[4]}\n"
+            all_std_textbox.insert('end', line)
+
+        all_tec_info.mainloop()
 
     def std_add(self):
 
@@ -105,14 +126,32 @@ class admin_panel:
                 error_l.update()
                 return
             else:
-                error_l.configure(text="")  # Clear error if no missing fields
+                error_l.configure(text="") 
 
-    # Store the data in a list
-            student_info = [username, name, std_class, roll, section, grade, phone, address]
+            # student_info = [username, name, std_class, roll, section, grade, phone, address]
 
-            ck = self.sql.add_student(student_info)
+            user_info = {'username': username,
+                         'pass': phone,
+                         'role': "student"
+            }
 
-            if ck == True:
+            student_info = {'username': username,
+                            's_name': name,
+                            'class': std_class,
+                            'roll': roll,
+                            'section': section,
+                            'grade': grade,
+                            'pn_number': phone,
+                            'address': address,
+                            'tution_fee': 1000,
+                            'paid_fee': 0
+            }
+
+            # sql backend
+            ck_s = self.sql.add_student(student_info)
+            ck_u = self.sql.add_users(user_info)
+
+            if ck_s == True and ck_u == True:
                 error_l.configure(text="Student Add Successfully", text_color="green")
                 error_l.update()
                 time.sleep(5)
@@ -120,7 +159,10 @@ class admin_panel:
                 error_l.update()
 
             else:
-                error_l.configure(text=ck, text_color="red")
+                if ck_s != True:
+                    error_l.configure(text=ck_s, text_color="red")
+                else:
+                    error_l.configure(text=ck_u, text_color="red")
                 error_l.update()
                 time.sleep(5)
                 error_l.configure(text="")
@@ -178,25 +220,125 @@ class admin_panel:
 
         self.add_std_slide_left(add_std_frame, int(4*263))
 
-    def std_find(self):
-        def std_find_result():
-            find_std_result_frame = CTkFrame(self.admin_frame, fg_color="transparent", width=700, height=350)
-            find_std_result_frame.place(x=4*263, y=260, anchor="center")
+    def std_find(self, delete, fees, tec):
 
-            header_label = CTkLabel(find_std_result_frame, text="Found", fg_color="transparent", width=1, height=1, font=("Helvetica",12, "bold"))
-            header_label.place(x=350, y=40, anchor="center")
+        def std_find_result():
+
+            def std_del(username):
+
+                if tec == 0:
+                    ck = self.sql.delete_student(username)
+                    if ck == True:
+                        header_label.configure(text="✅️ Delete Student Successfull", text_color="green")
+                        header_label.update
+                    else:
+                        header_label.configure(text=ck, text_color="red")
+                        header_label.update
+                
+                else:
+                    ck = self.sql.delete_teacher(username)
+                    if ck == True:
+                        header_label.configure(text="✅️ Delete Teacher Successfull", text_color="green")
+                        header_label.update
+                    else:
+                        header_label.configure(text=ck, text_color="red")
+                        header_label.update
+
+            find_std_result_frame = CTkFrame(self.admin_frame, fg_color="transparent", width=700, height=350)
+            find_std_result_frame.place(x=4*263, y=200, anchor="center")
+
+
+            header_label = CTkLabel(find_std_result_frame, text="", text_color="green", fg_color="transparent", width=1, height=1, font=("Helvetica",12, "bold"))
+            header_label.place(x=350, y=25, anchor="center")
+
+            show_s_f_result = CTkTextbox(find_std_result_frame, fg_color="transparent", width=300, height=220, font=("Helvetica",14, "bold"))
+            show_s_f_result.place(x=350, y=150, anchor="center")
+
+            find_s_username = e_s_username.get()
+
+            if find_s_username == "":
+                # e_s_username.update()
+                e_s_username.configure(placeholder_text_color="red", border_color="red")
+                # time.sleep(5)
+                # e_s_username.configure(text="")
+                # e_s_username.update()
+                return
+            else:
+                e_s_username.configure(placeholder_text_color="light_color", border_color="#979da2")
+            
+            # sql backend
+            if tec == 1:
+                s_f_result = self.sql.find_teacher(find_s_username)
+
+                if s_f_result is not None:
+                    header_label.configure(text="Found", text_color="green")
+                    header_label.update
+
+                    show_s_f_result.insert(END,
+                        f"ID : \t\t{s_f_result[0]}\nUsername : \t\t{s_f_result[1]}\n"
+                        f"Name : \t\t{s_f_result[2]}\nnPhone Number :\t\t{s_f_result[3]}\n"
+                        f"Address : \t\t{s_f_result[4]}\n"
+                    )
+                    show_s_f_result.configure(state="disable")
+
+                else:
+                    header_label.configure(text="Not Found", text_color="red")
+                    header_label.update
+
+
+            else:
+                s_f_result = self.sql.find_student(find_s_username)
+
+                if s_f_result is not None:
+                    header_label.configure(text="Found", text_color="green")
+                    header_label.update
+
+                    tuition_fee = float(s_f_result[9])
+                    paid_fee = float(s_f_result[10])
+                    due_fee = tuition_fee - paid_fee
+                    show_s_f_result.insert(END,
+                        f"ID : \t\t{s_f_result[0]}\nUsername : \t\t{s_f_result[1]}\n"
+                        f"Name : \t\t{s_f_result[2]}\nClass :\t\t{s_f_result[3]}\n"
+                        f"Roll : \t\t{s_f_result[4]}\nSection : \t\t{s_f_result[5]}\n"
+                        f"Grade : \t\t{s_f_result[6]}\nPhone Number : \t\t{s_f_result[7]}\n"
+                        f"Address : \t\t{s_f_result[8]}\nTution Fee : \t\t{s_f_result[9]}\n"
+                        f"Paid Fee : \t\t{s_f_result[10]}\nDue : \t\t{due_fee}\n"
+                    )
+                    show_s_f_result.configure(state="disable")
+
+                else:
+                    header_label.configure(text="Not Found", text_color="red")
+                    header_label.update
+
+
 
             self.add_std_slide_left(find_std_result_frame, int(4*263))
 
             e_s_back_btn = CTkButton(find_std_result_frame, text="❌", command=find_std_result_frame.destroy, width=2, height=30, corner_radius=500, font=("Helvetica",14, "bold"), hover=True)
             e_s_back_btn.place(x=670, y=20, anchor="center")
 
-            if self.delete == 1:
-                e_s_delete_btn = CTkButton(find_std_result_frame, text="Delete", command=self.std_add, font=("Helvetica",14, "bold"), hover=True)
+            if delete == 1:
+                # nonlocal find_s_username
+                e_s_delete_btn = CTkButton(find_std_result_frame, text="Delete", command=lambda: std_del(find_s_username), font=("Helvetica",14, "bold"), hover=True)
                 e_s_delete_btn.place(x=350, y=280, anchor="center")
-                self.delete = 0
+                # self.delete = 0
 
-            if self.fees == 1:
+            if fees == 1:
+
+                def up_fee():
+                    nonlocal find_s_username, header_label
+
+                    # sql backend
+                    update_fee = self.fee_update(find_s_username, f_d_entry.get(), f_p_entry.get())
+
+                    if update_fee == True:
+                        header_label.configure(text="Fees Update Successfull", text_color="green")
+                    else:
+                        header_label.configure(text="Fees Update Faild", text_color="red")
+
+
+                show_s_f_result.place(x=250, y=150, anchor="center")
+
                 update_fees_frame = CTkFrame(find_std_result_frame, width=200, height=200, fg_color="transparent", border_width=2, border_color="white")
                 update_fees_frame.place(x=580, y=150, anchor="center")
 
@@ -209,9 +351,10 @@ class admin_panel:
                 f_p_entry = CTkEntry(update_fees_frame, font=("Helvetica",14), placeholder_text="Now Payed", fg_color="transparent")
                 f_p_entry.place(x=100, y=115, anchor="center")
 
-                update_btn = CTkButton(update_fees_frame, text="Update", command=self.std_add, font=("Helvetica",14, "bold"), hover=True)
+                update_btn = CTkButton(update_fees_frame, text="Update", command=up_fee, font=("Helvetica",14, "bold"), hover=True)
                 update_btn.place(x=100, y=155, anchor="center")
-                self.fees = 0
+                # self.fees = 0
+
 
         find_std_frame = CTkFrame(self.admin_frame, fg_color="transparent", width=700, height=350)
         find_std_frame.place(x=4*263, y=260, anchor="center")
@@ -219,7 +362,10 @@ class admin_panel:
         e_s_username = CTkEntry(find_std_frame, font=("Helvetica",14), placeholder_text="Username", width=200, fg_color="transparent")
         e_s_username.place(x=350, y=100, anchor="center")
 
-        e_s_submit_btn = CTkButton(find_std_frame, text="Find", command=std_find_result, font=("Helvetica",14, "bold"), hover=True)
+        
+        # print(find_s_username)
+
+        e_s_submit_btn = CTkButton(find_std_frame, text="Find", command= lambda: std_find_result(), font=("Helvetica",14, "bold"), hover=True)
         e_s_submit_btn.place(x=350, y=180, anchor="center")
 
         e_s_back_btn = CTkButton(find_std_frame, text="❌", command=find_std_frame.destroy, width=2, height=30, corner_radius=500, font=("Helvetica",14, "bold"), hover=True)
@@ -227,11 +373,81 @@ class admin_panel:
 
         self.add_std_slide_left(find_std_frame, int(4*263))
 
-    def std_delete(self):
-        self.delete = 1
-        self.std_find()
+    def std_delete(self, delete, fees, tec):
+        self.std_find(delete, fees, tec)
 
     def tec_add(self):
+
+        # take std info and add database
+        def add_tec():
+            nonlocal e_s_username, e_s_name, e_s_class, e_s_sub, e_s_section, e_s_pn, e_s_adderss, error_l, e_ce_time, e_cs_time
+
+            # Retrieve all input values
+            username = e_s_username.get().strip()
+            name = e_s_name.get().strip()
+            std_class = e_s_class.get().strip()
+            sub = e_s_sub.get().strip()
+            section = e_s_section.get().strip()
+            phone = e_s_pn.get().strip()
+            class_st = e_cs_time.get().strip()
+            class_et = e_ce_time.get().strip()
+            address = e_s_adderss.get("1.0", "end-1c").strip()
+
+            # Check for missing fields
+            if not all([username, name, std_class, sub, section, phone, class_et, class_st]) or address == "Address":
+                error_l.configure(text="⚠️ All fields are required.")
+                error_l.update()
+                time.sleep(5)
+                error_l.configure(text="")
+                error_l.update()
+                return
+            else:
+                error_l.configure(text="") 
+
+            sub_info = { 'subject': sub,
+                        'username': username,
+                        'name': name,
+                        'class': std_class,
+                        'start_t': class_st,
+                        'end_t':class_et,
+                        'section': section
+            }
+
+            teacher_info = {'username': username,
+                            'name': name,
+                            'phone': phone,
+                            'address': address
+            }
+
+            user_info = {'username': username,
+                         'pass': phone,
+                         'role': "teacher"
+            }
+
+            # sql backend
+            ck_t = self.sql.add_teacher(teacher_info)
+            ck_s = self.sql.add_subject(sub_info)
+            ck_u = self.sql.add_users(user_info)
+
+            if (ck_t == True) and (ck_s == True) and (ck_u == True):
+                error_l.configure(text="Teacher Add Successfully", text_color="green")
+                error_l.update()
+                time.sleep(5)
+                error_l.configure(text="")
+                error_l.update()
+
+            else:
+                if ck_u != True:
+                    error_l.configure(text=ck_u, text_color="red")
+                elif ck_t != True:
+                    error_l.configure(text=ck_t, text_color="red")
+                else:
+                    error_l.configure(text=ck_s, text_color="red")
+                error_l.update()
+                time.sleep(5)
+                error_l.configure(text="")
+                error_l.update()
+
         def add_placeholder(event=None):
             if e_s_adderss.get("1.0", "end-1c").strip() == "":
                 e_s_adderss.insert("1.0", "Address")
@@ -251,14 +467,14 @@ class admin_panel:
         e_s_name.place(x=460, y=20+30, anchor="center")
         e_s_class = CTkEntry(add_tec_frame,font=("Helvetica",14), placeholder_text=" Class ", width=200, fg_color="transparent",)
         e_s_class.place(x=240, y=20+65, anchor="center")
-        e_s_roll = CTkEntry(add_tec_frame,font=("Helvetica",14), placeholder_text=" Subject ", width=200, fg_color="transparent",)
-        e_s_roll.place(x=460, y=20+65, anchor="center")
+        e_s_sub = CTkEntry(add_tec_frame,font=("Helvetica",14), placeholder_text=" Subject ", width=200, fg_color="transparent",)
+        e_s_sub.place(x=460, y=20+65, anchor="center")
         e_s_section = CTkEntry(add_tec_frame,font=("Helvetica",14), placeholder_text=" Section ", width=200, fg_color="transparent",)
         e_s_section.place(x=240, y=20+100, anchor="center")
-        e_cs_section = CTkEntry(add_tec_frame,font=("Helvetica",14), placeholder_text=" Class Start Time ", width=200, fg_color="transparent",)
-        e_cs_section.place(x=460, y=20+100, anchor="center")
-        e_es_section = CTkEntry(add_tec_frame,font=("Helvetica",14), placeholder_text=" Class End Time ", width=200, fg_color="transparent",)
-        e_es_section.place(x=460, y=20+135, anchor="center")
+        e_cs_time = CTkEntry(add_tec_frame,font=("Helvetica",14), placeholder_text=" Class Start Time ", width=200, fg_color="transparent",)
+        e_cs_time.place(x=460, y=20+100, anchor="center")
+        e_ce_time = CTkEntry(add_tec_frame,font=("Helvetica",14), placeholder_text=" Class End Time ", width=200, fg_color="transparent",)
+        e_ce_time.place(x=460, y=20+135, anchor="center")
         e_s_pn = CTkEntry(add_tec_frame,font=("Helvetica",14,),  placeholder_text=" Phone Number ", width=200, fg_color="transparent",)
         e_s_pn.place(x=240, y=20+135, anchor="center")
         e_s_adderss = CTkTextbox(add_tec_frame,font=("Helvetica",14,), wrap="word", width=420, height=65, fg_color="transparent", corner_radius=6, border_width=2)
@@ -267,38 +483,41 @@ class admin_panel:
         e_s_adderss.bind("<FocusIn>", remove_placeholder)
         e_s_adderss.bind("<FocusOut>", add_placeholder)
 
-        e_t_submit_btn = CTkButton(add_tec_frame, text="ADD Teacher", command=self.std_add, font=("Helvetica",14, "bold"), hover=True,)
+        e_t_submit_btn = CTkButton(add_tec_frame, text="ADD Teacher", command= add_tec , font=("Helvetica",14, "bold"), hover=True,)
         e_t_submit_btn.place(x=350, y=280, anchor="center")
 
         e_s_back_btn = CTkButton(add_tec_frame, text="❌", command=add_tec_frame.destroy, width=2, height=30, corner_radius=500, font=("Helvetica",14, "bold"), hover=True,)
         e_s_back_btn.place(x=670, y=20, anchor="center")
 
+        error_l = CTkLabel(self.admin_frame, text="", height=1, width=1, fg_color="transparent", text_color="red")
+        error_l.place(x=350, y=100, anchor="center")
+
         self.add_std_slide_left(add_tec_frame, int(4*263))
     
 
-    def tec_find(self):
-        self.std_find()
+    def tec_find(self, delete, fees, tec):
+        self.std_find(delete, fees, tec)
 
-    def tec_delete(self):
-        self.std_delete()
+    def tec_delete(self, delete, fees, tec):
+        self.std_delete(delete, fees, tec)
 
-    def fee_update(self):
-        self.fees = 1
-        self.std_find()
+    def fee_update(self, delete, fees, tec):
+        self.std_find(delete, fees, tec)
 
     def setup_admin_ui(self):
         admin_label = CTkLabel(self.admin_frame, text=f"Admin\nHi! {self.username}", width=1, height=1, fg_color="transparent", text_color="black", font=("Helvetica",22, "bold"))
         admin_label.place(x=350, y=50, anchor="center")
 
         buttons = [
-            ("ADD Student", self.std_add, 250, 150),
-            ("Find Student", self.std_find, 250, 200),
-            ("Delete Student", self.std_delete, 250, 250),
-            ("Update Fees", self.fee_update, 250, 300),
-            ("ADD Teacher", self.tec_add, 450, 150),
-            ("Find Teacher", self.tec_find, 450, 200),
-            ("Delete Teacher", self.tec_delete, 450, 250),
-            ("See All Student Info", self.see_all_std_info, 450, 300),
+            ("ADD Student", self.std_add, 250, 130),
+            ("Find Student", lambda: self.std_find(0, 0, 0), 250, 180),
+            ("Delete Student", lambda: self.std_delete(1, 0, 0), 250, 230),
+            ("See All Student Info", self.see_all_std_info, 250, 280),
+            ("ADD Teacher", self.tec_add, 450, 130),
+            ("Find Teacher", lambda: self.tec_find(0, 0, 1), 450, 180),
+            ("Delete Teacher", lambda: self.tec_delete(1, 0, 1), 450, 230),
+            ("See All Teacher Info", self.see_all_tec_info, 450, 280),
+            ("Update Fees", lambda: self.fee_update(0, 1, 0), 350, 330)
         ]
 
         for text, command, x, y in buttons:
--- sql_query.py
+++ sql_query.py
@@ -11,16 +11,35 @@ class MySQLQuery:
         )
         self.cursor = self.db.cursor()
 
-    # 1. Add Student
-    def add_student(self, student_data):
+
+    def add_users(self, user_info):
         try:
-            sql = """
-            INSERT INTO students 
-            (username, s_name, class, roll, section, grade, pn_number, address, tution_fee, paid_fee) 
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, 1000, 0);
-            """
-            self.cursor.execute(sql, student_data)
+            sql = """INSERT INTO users (username, password, role)
+            VALUES (%s, %s, %s);"""
+
+            values = (user_info['username'], user_info['pass'], user_info['role'] )
+            
+            self.cursor.execute(sql, values)
             self.db.commit()
+
+            return True
+        except mysql.connector.IntegrityError as e:
+            return f"Error : {e}"
+        except Exception as e:
+            return f"Error : {e}"
+        pass
+
+    # 1. Add Student
+    def add_student(self, student_data: dict):
+        try:
+            sql = """INSERT INTO students (username, s_name, class, roll, section, grade, pn_number, address, tution_fee, paid_fee)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+
+            values = (student_data['username'], student_data['s_name'], student_data['class'], student_data['roll'], student_data['section'], student_data['grade'], student_data['pn_number'], student_data['address'], student_data['tution_fee'], student_data['paid_fee'] )
+            
+            self.cursor.execute(sql, values)
+            self.db.commit()
+
             return True
         except mysql.connector.IntegrityError as e:
             return f"Error : {e}"
@@ -30,49 +49,48 @@ class MySQLQuery:
 
     # 2. Find Student
     def find_student(self, username):
-        self.cursor.execute("SELECT * FROM students WHERE username = %s", (username,))
-        return self.cursor.fetchone()
+        self.cursor.execute("SELECT * FROM students WHERE username = %s;", (username,))
+        f_s = self.cursor.fetchone()
+        return f_s
+        
+
 
     # 3. Delete Student
     def delete_student(self, username):
         try:
-            self.cursor.execute("DELETE FROM students WHERE username = %s", (username,))
-            return True
+            ck = self.find_student(username)
+            if ck is not None:
+                self.cursor.execute("DELETE FROM students WHERE username = %s;", (username,))
+                self.db.commit()
+                return True
+            else:
+                return "❌ Student Not Found"
+            
         except Exception as e:
-            print(f"[Error] Failed to delete student: {e}")
-            return False
+            return f"❌ [Error] Failed to delete student: {e}"
 
-    # 4. Student Info
-    def student_info(self, username):
-        self.cursor.execute("SELECT name, grade, section, roll FROM students WHERE username = %s", (username,))
-        return self.cursor.fetchone()
-
-    # 5. All Students
+    # 4. All Students
     def all_students(self):
-        self.cursor.execute("SELECT * FROM students")
+        self.cursor.execute("SELECT * FROM students;")
         return self.cursor.fetchall()
 
-    # 6. Student Routine
+    # 5. Student Routine
     def student_routine(self, username):
         self.cursor.execute("SELECT subject_name, class, section, class_start_time, class_end_time FROM subject WHERE username = %s", (username,))
         return self.cursor.fetchall()
 
-    # 7. Add Teacher
+    # 6. Add Teacher
     def add_teacher(self, teacher_data: dict):
-        required_fields = ['username', 'name', 'phone', 'address']
-        missing_fields = [field for field in required_fields if field not in teacher_data]
-        if missing_fields:
-            print(f"[Error] Missing teacher fields: {', '.join(missing_fields)}")
-            return False
-
+        
         try:
-            sql = """INSERT INTO teachers (username, name, phone, address)
+            sql = """INSERT INTO teacher (username, t_name, t_pn_number, t_address)
                      VALUES (%s, %s, %s, %s)"""
             values = (
                 teacher_data['username'], teacher_data['name'],
                 teacher_data['phone'], teacher_data['address']
             )
             self.cursor.execute(sql, values)
+            self.db.commit()
             return True
         except mysql.connector.IntegrityError as e:
             print(f"[Error] Failed to add teacher: {e}")
@@ -81,36 +99,73 @@ class MySQLQuery:
             print(f"[Error] Unexpected error: {e}")
             return False
 
-    # 8. Find Teacher
+    # 7. Find Teacher
     def find_teacher(self, username):
-        self.cursor.execute("SELECT * FROM teachers WHERE username = %s", (username,))
-        return self.cursor.fetchone()
+        self.cursor.execute("SELECT * FROM teacher WHERE username = %s", (username,))
+        f_t = self.cursor.fetchone()
+        return f_t
 
-    # 9. Delete Teacher
+    # 8. Delete Teacher
     def delete_teacher(self, username):
         try:
-            self.cursor.execute("DELETE FROM teachers WHERE username = %s", (username,))
-            return True
+            ck = self.find_teacher(username)
+            if ck is not None:
+                self.cursor.execute("DELETE FROM teacher WHERE username = %s;", (username,))
+                self.db.commit()
+                return True
+            else:
+                return "❌ Teacher Not Found"
+            
         except Exception as e:
-            print(f"[Error] Failed to delete teacher: {e}")
-            return False
+            return f"❌ [Error] Failed to delete student: {e}"
 
-    # 10. Teacher Info
+    # 9. Teacher Info
     def teacher_info(self, username):
-        self.cursor.execute("SELECT name, phone FROM teachers WHERE username = %s", (username,))
+        self.cursor.execute("SELECT name, phone FROM teacher WHERE username = %s", (username,))
         return self.cursor.fetchone()
 
-    # 11. All Teachers
+    # 10. All Teachers
     def all_teachers(self):
-        self.cursor.execute("SELECT * FROM teachers")
+        self.cursor.execute("SELECT * FROM teacher;")
         return self.cursor.fetchall()
 
-    # 12. Teacher Routine
+    # 11. Teacher Routine
     def teacher_routine(self, username):
         self.cursor.execute("SELECT subject_name, class, section, class_start_time, class_end_time FROM subject WHERE username = %s", (username,))
         return self.cursor.fetchall()
+    
+    def add_subject(self, sub_info: dict):
 
-    # 13. Student Fee
+        try:
+            sql = """INSERT INTO subjects (sub_name, username, t_name, class, class_start_time, class_end_time, section)
+                     VALUES (%s, %s, %s, %s, %s, %s, %s);"""
+            values = (
+                sub_info['subject'],sub_info['username'], sub_info['name'], 
+                sub_info['class'], sub_info['start_t'], sub_info['end_t'],sub_info['section']
+            )
+            self.cursor.execute(sql, values)
+            self.db.commit()
+            return True
+        except mysql.connector.IntegrityError as e:
+            print(f"[Error] Failed to add teacher: {e}")
+            return False
+        except Exception as e:
+            print(f"[Error] Unexpected error: {e}")
+            return False
+
+
+    # 12. Student Fee
     def student_fee(self, username):
         self.cursor.execute("SELECT tuition_fee, paid_fee, (tuition_fee - paid_fee) AS due_fee FROM students WHERE username = %s", (username,))
         return self.cursor.fetchone()
+    
+    def update_fees(self, username, tution_fee, paid_fee):
+        try:
+            self.cursor.execute("UPDATE students SET tution_fees = %s, paid_fees = %s WHERE = %s", (tution_fee, paid_fee, username,))
+            self.db.commit()
+            return True
+        except:
+            return False
+
+
+# UPDATE table_name SET column_name = new_value WHERE condition;
